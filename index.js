09d08204facfd666fa719f152fc1ab96fb908a6e
basilcea <ogbonna.basil3@gmmail.com>
2019-07-10 14:51:25 +0100

    create validateUser and validatePost middleware

--- index.js
+++ index.js
@@ -12,3 +12,34 @@ const validateUserId = async (req , res , next) => {
     return res.status(500).json(err.toString())
 }
 }
+
+const validateUser = async (req , res , next) => {
+    const {name} = req.body
+   try{
+    if(!req.body){
+        return res.status(400).json({message: "missing user data"})      
+    }
+    if(!name){
+        return res.status(400).json({message: "missing required name field"})  
+    }
+    next()
+}catch(err){
+    return res.status(500).json(err.toString())
+}
+}
+
+const validatePost = async (req , res , next) => {
+    const {text} = req.body
+   try{
+    if(!req.body){
+        return res.status(400).json({message: "missing post data"})      
+    }
+    if(!text){
+        return res.status(400).json({message: "missing required text field"})  
+    }
+    next()
+}catch(err){
+    return res.status(500).json(err.toString())
+}
+}
+
