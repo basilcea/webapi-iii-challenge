9bfb6abd00581975417bab1f89ffdf4f9ccb4773
basilcea <ogbonna.basil3@gmmail.com>
2019-07-10 14:18:18 +0100

    create custom middleware -logger

--- server.js
+++ server.js
@@ -1,15 +1,25 @@
-const express = 'express';
+const express = "express";
 
 const server = express();
 
-server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`)
+server.use(express.json());
+server.use(express.urlencoded({ extended: true }));
+
+server.get("/", (req, res) => {
+  res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
 
 function logger(req, res, next) {
+  console.log({
+    Request_Method: req.method,
+    Request_Url: req.url,
+    Timestamp: new Date().toISOString(),
+    Origin: req.get("Origin")
+  });
 
-};
+  next();
+}
 
 module.exports = server;
